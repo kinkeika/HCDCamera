2d1806c4f3e09f181fe0e289a4a885241e9586ca
cheaterhu <724300087@qq.com>
2016-09-06 17:49:15 +0800

    first commit

---
+++ HCDCamera/HCDCamera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HCDCamera
+//
+//  Created by cheaterhu on 16/9/6.
+//  Copyright © 2016年 hcd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
