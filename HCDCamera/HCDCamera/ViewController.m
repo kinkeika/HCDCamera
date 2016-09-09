adb548ba2f1d68506febf8eadcd087b5b9c390aa
cheaterhu <724300087@qq.com>
2016-09-09 17:04:42 +0800

    视频拍摄支持（待优化）

--- HCDCamera/HCDCamera/ViewController.m
+++ HCDCamera/HCDCamera/ViewController.m
@@ -28,8 +28,12 @@
 @interface ViewController ()
 {
     NSInteger selectedIndex;
+    BOOL isPhotoMode;
 }
 @property (nonatomic, strong) GPUImageVideoCamera *videoCamera;
+@property (nonatomic, strong) GPUImageMovieWriter *movieWriter;
+@property (nonatomic, strong) NSString *moviePath;
+
 @property (nonatomic, strong) GPUImageView *filterView;
 
 @property (nonatomic, strong) UIButton *captureButton;
@@ -55,6 +59,8 @@
 
     [self p_initCamera];
     [self p_initUI];
+    
+    [[NSFileManager defaultManager] removeItemAtPath:self.moviePath error:nil];
     
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(appDidEnterBackground) name:UIApplicationDidEnterBackgroundNotification object:nil];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(appWillEnterForeground) name:UIApplicationWillEnterForegroundNotification object:nil];
@@ -150,7 +156,7 @@
     //底下操作按钮 ==========
     
     self.captureButton = [UIButton buttonWithMyType];
-    [self.captureButton setTitle:@"拍摄" forState:UIControlStateNormal];
+    [self.captureButton setTitle:@"拍照" forState:UIControlStateNormal];
     [self.captureButton addTarget:self action:@selector(capture) forControlEvents:UIControlEventTouchUpInside];
     self.captureButton.layer.cornerRadius = 30.0;
     self.captureButton.backgroundColor = [UIColor colorWithRGBIntegerString:@"172-218-224"];
@@ -183,7 +189,7 @@
     //照片  视频
     UIButton *modeButton = [UIButton buttonWithMyType];
     [modeButton setTitle:@"视频" forState:UIControlStateNormal];
-    [modeButton addTarget:self action:@selector(changeMode) forControlEvents:UIControlEventTouchUpInside];
+    [modeButton addTarget:self action:@selector(changeMode:) forControlEvents:UIControlEventTouchUpInside];
     modeButton.backgroundColor = [UIColor colorWithRGBIntegerString:@"162-207-125"];
     [modeButton setBackgroundImage:[UIImage imageWithColor:[UIColor colorWithRGBIntegerString:@"162-207-125"]] forState:UIControlStateNormal];
     
@@ -204,11 +210,11 @@
 
 - (void)p_initCamera
 {
-    self.videoCamera = [[GPUImageVideoCamera alloc] initWithSessionPreset:AVCaptureSessionPreset1920x1080 cameraPosition:AVCaptureDevicePositionBack];
+    self.videoCamera = [[GPUImageVideoCamera alloc] initWithSessionPreset:AVCaptureSessionPresetiFrame1280x720 cameraPosition:AVCaptureDevicePositionBack];
     self.videoCamera.outputImageOrientation = UIInterfaceOrientationPortrait;
     self.videoCamera.horizontallyMirrorFrontFacingCamera = YES;
     
-    self.filterView = [[GPUImageView alloc] initWithFrame:self.view.frame];
+    self.filterView = [[GPUImageView alloc] initWithFrame:self.view.bounds];
     self.filterView.center = self.view.center;
     
     [self.view addSubview:self.filterView];
@@ -216,7 +222,12 @@
     
     self.currentFilter = self.filters[0];
     selectedIndex = 1000;
-    [self.currentFilter useNextFrameForImageCapture];
+    isPhotoMode = YES;
+    
+    if (isPhotoMode) {
+      [self.currentFilter useNextFrameForImageCapture];
+    }
+    
     [self.videoCamera addTarget:self.currentFilter];
     [self.videoCamera startCameraCapture];
 }
@@ -225,26 +236,53 @@
 
 - (void)capture
 {
-    [self.videoCamera stopCameraCapture];
-    
-    self.captureButton.enabled = NO;
-    UIImage *image = nil;
-    
-    if (!self.currentFilter) {
-        self.currentFilter = [self.filters firstObject];
+    if (isPhotoMode) {
+        [self.videoCamera stopCameraCapture];
+        self.captureButton.enabled = NO;
+        UIImage *image = nil;
+        
+        if (!self.currentFilter) {
+            self.currentFilter = [self.filters firstObject];
+        }
+        
+        image = [self.currentFilter imageFromCurrentFramebuffer];
+        
+        self.imageView.image = image;
+        self.imageView.hidden = NO;
+        
+        UIImageWriteToSavedPhotosAlbum(self.imageView.image, self, @selector(image:didFinishSavingWithError:contextInfo:), NULL);
+    }else{
+        if ([self.captureButton.titleLabel.text isEqualToString:@"录制"]) {
+            [self.captureButton setTitle:@"停止" forState:UIControlStateNormal];
+            
+            self.videoCamera.captureSessionPreset = AVCaptureSessionPreset640x480;
+            [self.currentFilter addTarget:self.movieWriter];
+            self.videoCamera.audioEncodingTarget = self.movieWriter;
+            [self.movieWriter startRecording];
+            
+        }else if ([self.captureButton.titleLabel.text isEqualToString:@"停止"]){
+            [self.videoCamera stopCameraCapture];
+            [self.currentFilter removeTarget:self.movieWriter];
+            
+            __weak typeof(self) weakSelf = self;
+            [self.movieWriter finishRecordingWithCompletionHandler:^{
+                UISaveVideoAtPathToSavedPhotosAlbum(weakSelf.moviePath, weakSelf, @selector(video:didFinishSavingWithError:contextInfo:), NULL);
+            }];
+        }
     }
-    
-    image = [self.currentFilter imageFromCurrentFramebuffer];
-    
-    self.imageView.image = image;
-    self.imageView.hidden = NO;
-    
-    UIImageWriteToSavedPhotosAlbum(self.imageView.image, self, @selector(image:didFinishSavingWithError:contextInfo:), NULL);
 }
 
-- (void)changeMode
+- (void)changeMode:(UIButton *)sender
 {
+    isPhotoMode = !isPhotoMode;
     
+    if (isPhotoMode) {
+        [self.captureButton setTitle:@"拍照" forState:UIControlStateNormal];
+        [sender setTitle:@"视频" forState:UIControlStateNormal];
+    }else{
+        [self.captureButton setTitle:@"录制" forState:UIControlStateNormal];
+        [sender setTitle:@"照片" forState:UIControlStateNormal];
+    }
 }
 
 - (void)changeInput
@@ -262,12 +300,30 @@
     
     [self.videoCamera removeAllTargets];
     self.currentFilter = self.filters[sender.tag - 1000];
-    [self.currentFilter useNextFrameForImageCapture];
+    
+    if (isPhotoMode) {
+        [self.currentFilter useNextFrameForImageCapture];
+    }
+    
     [self.videoCamera addTarget:self.currentFilter];
     [self.currentFilter addTarget:self.filterView];
 }
 
-#pragma mark - save image callback
+- (BOOL)shouldAutorotate{
+    
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+    self.videoCamera.outputImageOrientation = self.interfaceOrientation;
+#pragma clang diagnostic pop
+    return YES;
+}
+
+-(void)viewWillLayoutSubviews
+{
+    self.filterView.frame = self.view.bounds;
+}
+
+#pragma mark - save image/video callback
 
 - (void)image:(UIImage *) image didFinishSavingWithError: (NSError *) error contextInfo: (void *) contextInfo {
     
@@ -283,13 +339,52 @@
     }
 }
 
+- (void)video:(NSString *)videoPath didFinishSavingWithError:(NSError *)error contextInfo:(void *)contextInfo {
+    
+    __weak typeof(self) weakSelf = self;
+    if(error == nil) {
+        [weakSelf.captureButton setTitle:@"录制" forState:UIControlStateNormal];
+        
+        dispatch_async(dispatch_get_global_queue(0, DISPATCH_QUEUE_PRIORITY_DEFAULT), ^{
+            
+            NSError *error = nil;
+            [[NSFileManager defaultManager] removeItemAtPath:self.moviePath error:&error];
+            
+            HCDLog(@"%@======",error.localizedDescription);
+        });
+        
+        [HCDIndicatorView showProgressInView:self.view completeHandler:^{
+            weakSelf.captureButton.enabled = YES;
+            
+            weakSelf.imageView.hidden = YES;
+            weakSelf.movieWriter = nil;
+            [weakSelf.videoCamera startCameraCapture];
+        }];
+    }
+}
+
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
-    [self.videoCamera stopCameraCapture];
+//    [self.videoCamera stopCameraCapture];
 }
 
 #pragma mark - getter
+
+- (GPUImageMovieWriter *)movieWriter
+{
+    if(!_movieWriter){
+        _movieWriter = [[GPUImageMovieWriter alloc] initWithMovieURL:[NSURL fileURLWithPath:self.moviePath] size:CGSizeMake(640, 480) fileType:AVFileTypeMPEG4 outputSettings:nil];
+        _movieWriter.encodingLiveVideo = YES;
+    }
+    return _movieWriter;
+}
+
+- (NSString *)moviePath
+{
+    return [NSString stringWithFormat:@"%@myMoviewName.mp4", NSTemporaryDirectory()];
+}
 
 - (NSArray *)filters
 {
